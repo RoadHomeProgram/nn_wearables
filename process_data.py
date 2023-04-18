a296e9177b5bb58ba6464cfb2d48caa5feeb7e10
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2023-04-18 13:48:29 -0500

    function to identify files that contain data + labels

--- process_data.py
+++ process_data.py
@@ -1,14 +1,23 @@
-
 import pandas as pd
 import numpy as np
 from datetime import datetime
 import os
+import re
 
 def identify_source_files(path="/Users/ryanschubert/Dropbox (Rush)/TREAT Lab/Wearables/Data/"):
     folders=os.listdir(path)
+    folders=[f for f in folders if re.search('^([1-9])+(_)', f)]
+    prefixes=[]
     for i  in enumerate(folders):
-        print(i)
-    return
+        files = os.listdir(path + i[1])
+        files = [re.sub('\\.([a-zA-Z_-])+\\.([a-z])+$','',x) for x in files]
+        files = set(files)
+        files = [path + i for i in files]
+        prefixes.append(files)
+    prefixes = [file for sublist in prefixes for file in sublist]
+    measurement_prefixes=[i for i in prefixes if re.search('([0-9]){8,8}$',i)]
+    survey_prefixes=[i for i in prefixes if re.search('([0-9]){,8}(T)([0-9])+$',i)]
+    return measurement_prefixes, survey_prefixes
 
 
 def readInPPG(path):
