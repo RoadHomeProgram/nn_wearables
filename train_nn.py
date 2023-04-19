9576aa2f789402aac4a557be9152a5155541631a
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2023-04-19 14:11:11 -0500

    initial params for nn

--- train_nn.py
+++ train_nn.py
@@ -28,16 +28,20 @@ class myCallback(tf.keras.callback.Callback):
             print("\nTarget loss achieved!")
             self.model.stop_training = True
 
-
+n_steps=1000
+n_features=2
 ##define neural net
 #lets start with a simple version
 #need to define the input shapes, which will depend on the actual data
 #The configuration of the 1D CNN used in all experiments has 3 hidden convolutional layers and 2 dense layers. The 1D CNNs have 32 and 16 neurons on the first and second hidden convolutional layers and 10 neurons on the hidden dense layer. The output layer size is 5 which is the number of beat classes and the input (CNN) layer size is either 2 (base) or 4 (extended) according to the choice of raw data representation. For 64 and 128 sample beat representations, the kernel sizes are set to 9 and 15, and the sub-sampling factors are set to 4 and 6, respectively
 def define_model():
     model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv1D(),#need to decide how many filters and define the input shape
-            tf.keras.layers.MaxPool1D(), #need to decide 
-            tf.keras.layers.Dense(), #do i need a flatten layer?
+            tf.keras.layers.Conv1D(32, kernel_size=2, activation='relu', input_shape=(n_steps, n_features)),#need to decide how many filters and define the input shape
+            tf.keras.layers.Conv1D(16, kernel_size=2, activation='relu'),#need to decide how many filters and define the input shape
+            tf.keras.layers.MaxPool1D(pool_size=2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(10,activation='relu'),
+            tf.keras.layers.Dense(11,activation='softmax'),
         ])
 
     model.compile(optimizer='adam',
